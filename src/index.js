454d13bbbfa4921745cfc6b4f6ca350d700cfae2
AsadMughalAM <am127955@gmail.com>
2025-08-19 12:56:26 +0500

    connect to mongodb

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({path:"./env"})
+connectDB( )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// require('dotenv').config({path:'./env'})
+// import mongoose from "mongoose";
+// import express from 'express'
+// import {DB_NAME} from './constants.js'
+
+// const app=express()
+// // IIFE
+// ;(async()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//        app.on("Error",(error)=>{
+//         console.log("Error",error)
+//         throw err
+//        })
+
+//        app.listen(process.env.PORT || 4000,()=>{
+//         console.log(`app listen on port ${process.env.PORT}`)
+//        })
+       
+//     } catch (error) {
+//         console.log("ERROR",error)
+//         throw err   
+//     }
+// })()
+
